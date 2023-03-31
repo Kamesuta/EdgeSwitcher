46b7fb9fd122be713e01854603ce0eb2ac3e4ba1
Kamesuta <kamesuta@gmail.com>
2023-04-01 05:35:41 +0900

    マスク方式に変更

--- Assets/Palette/LineGridSystem.cs
+++ Assets/Palette/LineGridSystem.cs
@@ -17,6 +17,9 @@ public class LineGridSystem : MonoBehaviour
     // 途中タイルマップ
     public Tilemap partialTilemap;
 
+    // 途中タイルマップマスク
+    public Transform partialMask;
+
     // 横タイル
     public TileBase horizontalTilebase;
 
@@ -47,25 +50,28 @@ public class LineGridSystem : MonoBehaviour
     {
         // 途中タイルの位置
         Vector3 centerPos = baseGrid.CellToWorld((Vector3Int)centerCell);
-        // 途中タイルマップをクリア
-        partialTilemap.ClearAllTiles();
+        // 半分グリッドサイズ
+        var half = (Vector2)baseGrid.cellSize / 2f;
+
         // 横線
         if (moveDirection.y == 0)
         {
             int sideOffset = (sideDirection.y - 1) / 2;
             int moveOffset = (moveDirection.x - 1) / 2;
-            partialTilemap.SetTile(new Vector3Int(moveOffset, 0), horizontalTilebase);
-            partialTilemap.transform.position = centerPos + new Vector3(-moveOffset, sideOffset + 0.5f, 0);
-            partialTilemap.transform.localScale = new Vector3(partialPercent, 1, 1);
+            partialTilemap.SetTile(Vector3Int.zero, horizontalTilebase);
+            partialTilemap.transform.position = centerPos + new Vector3(0, sideOffset + half.y, 0);
+            partialMask.transform.localPosition = new Vector3(partialPercent * moveDirection.x * half.x - moveOffset, half.y);
+            partialMask.transform.localScale = new Vector3(partialPercent, 1, 1);
         }
         // 縦線
         if (moveDirection.x == 0)
         {
             int sideOffset = (sideDirection.x - 1) / 2;
             int moveOffset = (moveDirection.y - 1) / 2;
-            partialTilemap.SetTile(new Vector3Int(0, moveOffset), verticalTilebase);
-            partialTilemap.transform.position = centerPos + new Vector3(sideOffset + 0.5f, -moveOffset, 0);
-            partialTilemap.transform.localScale = new Vector3(1, partialPercent, 1);
+            partialTilemap.SetTile(Vector3Int.zero, verticalTilebase);
+            partialTilemap.transform.position = centerPos + new Vector3(sideOffset + half.x, 0, 0);
+            partialMask.transform.localPosition = new Vector3(half.x, partialPercent * moveDirection.y * half.y - moveOffset);
+            partialMask.transform.localScale = new Vector3(1, partialPercent, 1);
         }
     }
 }
